f56deb86a6f4e45a861ef6c0bb3572e4834140a6
Tracy Dong <dongtra@sheridancollege.ca>
2020-05-24 15:44:40 -0400

    finished retrieval functions! :)

---
+++ library.py
@@ -0,0 +1,8 @@
+""" main library module """
+import csv
+import tkinter
+import retrieval
+
+def writetocsv(data):
+    """ write to books csv """
+    pass
--- retrieval.py
+++ retrieval.py
@@ -1,8 +1,7 @@
 """
 retrieves book data from google books api
-
+all requests are made here!
 """
-
 import json
 import requests
 
@@ -16,20 +15,31 @@ def test_connection(url):
     request = getrequest(url)
     return request.status_code == 200
 
-def id_retrieve(BookID): #finished
+def id_retrieve(bookid):
     """ retrieve and return first google book result data"""
-    url = "https://www.googleapis.com/books/v1/volumes?q=id="+BookID
-    response = getrequest(url)
-    unfiltered = json.loads(response.content)['items'][0]['volumeInfo']
-    return unfiltered
+    url = "https://www.googleapis.com/books/v1/volumes?q=id="+bookid
+    if test_connection(url):
+        response = getrequest(url)
+        unfiltered = json.loads(response.content)['items'][0]['volumeInfo']
+        return unfiltered
+    return "error"
 
-def search(term, type):
-    """ search and return first 10 results """
-    pass
+def search(customised, sliced=10):
+    """ search and return 10 most relevant results **can be modified"""
+    url = "https://www.googleapis.com/books/v1/volumes?q="+customised
+    if test_connection(url):
+        response = getrequest(url)
+        unfiltered = json.loads(response.content)
+        lst = []
+        for i in range(sliced):
+            lst.append(unfiltered['items'][i]['volumeInfo'])
+        return lst
+    return "error"
 
 def main():
     """ testing functions :) """
     id_retrieve('buc0AAAAMAAJ')
+    search('flowers')
 
 if __name__ == "__main__":
     main()
