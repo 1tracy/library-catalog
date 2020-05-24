2e6d78215f3a6a152f84c18e559b730ec45b5be3
Tracy Dong <dongtra@sheridancollege.ca>
2020-05-24 17:08:41 -0400

    edit

--- library.py
+++ library.py
@@ -1,5 +1,6 @@
 """ main library module """
 import csv
+import datetime
 from tkinter import *
 
 from retrieval import general_search
@@ -9,16 +10,44 @@ class Account:
     """ library user account """
     def __init__(self):
         self._borrowed = []
-        self._fees = 0
+        self._fees = _feecalculator(self._borrowed)
         self._favorites = []
+
+    @classmethod
+    def _feecalculator (cls, lst):
+        """ calculate fees -> $1/overdue day, two week borrow period"""
+        pass
+
+    def borrow_book(self, name, id):
+        """ borrow book given name and google book id (for easy referencing) """
+        string = [id, datetime.datetime.now()]
+        self._borrowed.append({name: string})
+
+    def return_book(self, index):
+        """ return book given index"""
+        try:
+            self._borrowed.pop(index)
+        except KeyError:
+            return "this book does not exist"
+
+    def renew_book(self, index):
+        """ renew borrowing period for already borrowed book """
+        temp = self._borrowed[index]
+        del self._borrowed[index]
+        #self._borrowed.append({list(temp.keys())[0]: [list(temp.values())})
+
 
 def writetocsv(data):
     """ write transaction to books csv """
     pass
 
 def sortdata(lst):
-    """ sort data from retrieval file """
-    pass
+    """ sort data from singular entry only"""
+    temp = {'title': lst['title'],
+            'authors': lst['authors'],
+            'publisher': lst['publisher'],
+            'description': lst['description']}
 ##TKINTER CODE BELOW
 
-print(general_search('flowers'))
+unsorted = general_search('flowers')
+sortdata(unsorted[0])
